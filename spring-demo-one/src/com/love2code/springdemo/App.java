22215588ae49f133f23805a7e21793ca945989ed
SelectionSort <yihongmse@gmail.com>
2020-03-04 11:47:58 -0500

    2nd commit

---
+++ spring-demo-one/src/com/love2code/springdemo/App.java
@@ -0,0 +1,11 @@
+package com.love2code.springdemo;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
---
+++ spring-demo-one/src/com/love2code/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.love2code.springdemo;
+
+public class BaseballCoach {
+	
+	public String getDailyWorkout() {
+		
+		return "Spend 30 mins on batting practice";
+	}
+
+}
